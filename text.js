1737e0e0f091a823ed77e59f6079da085c1f88d2
AlexGithubbb <31776215+AlexGithubbb@users.noreply.github.com>
2019-08-12 21:21:47 -0400

    remove proxy and node server port 5000 from json file

--- text.js
+++ text.js
@@ -4,3 +4,11 @@
   //   "json-server": "json-server --watch db.json --port 5000",
   //   "dev": "concurrently npm:start npm:json-server" // <-------- Instead of "concurrently \"npm start\" \"npm run json-server\""
   // },
+
+
+
+  // for running json server, you should add those following code in package.json
+// "proxy": "http://localhost:5000"
+// scripts:
+// "json-server": "json-server --watch db.json --port 5000",
+//   "dev": "concurrently \"npm start\" \"npm run json-server\""
